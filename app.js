e77d5e72716b757fb5e0802f1e99e7df859821d6
AndPan3 <andrejbajusic2012@gmail.com>
2026-09-02 23:06:02 +0200

    Connected HTML and JS

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-var number = Math.floor(Math.random() * 15)+1;
-window.location.href = `https://andpan3.github.io/random-song-hardmusic/sites/${number}.html`;
+const button = document.getElementById("button")
+button.addEventListener("click",function randomnumber(){var number = Math.floor(Math.random() * 10)+1;
+    window.location.href = `https://andpan3.github.io/random-song-hardmusic/sites/${number}.html`;
+})
